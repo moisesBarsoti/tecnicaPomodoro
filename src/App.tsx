f580ed2ad84af671de63144d5a97f58d6f91e048
moisesBarsoti <moises.barsot2@gmail.com>
2024-12-06 19:07:10 -0300

    Subindo Arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import "./App.css";
 export const App = () => {
   return (
     <div className="container">
-      <div className="colorBgWorking">
         <Pomodorotimer
           pomodoroTime={1500}
           shortRestTime={300}
           longRestTime={900}
           cycles={4}
         />
-      </div>
     </div>
   );
 };
--- src/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useInterval } from "../../hooks/useInterval";
 import { Button } from "../Botao/Botao";
 import { Timer } from "../Timer/Timer";
@@ -13,19 +13,51 @@ interface Props {
 
 export const Pomodorotimer = (props: Props): JSX.Element => {
   const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
-  }, 1000);
+  }, 
+  timeCounting ? 1000 : null,
+);
+
+useEffect(() => {
+    if(working) document.body.classList.add('colorBgWorking');
+    if(resting) document.body.classList.remove('colorBgWorking');
+}, [working]);
+
+const configureWork = () => {
+  setTimeCounting(true);
+  setWorking(true);
+  setResting(false);
+  setMainTime(props.pomodoroTime)
+}
+
+const configureResting = (long: boolean) => {
+  setTimeCounting(true);
+  setWorking(false);
+  setResting(true);
+
+  if(long) {
+    setMainTime(props.longRestTime);
+  } else {
+    setMainTime(props.shortRestTime)
+  }
+}
 
   return (
     <div className="pomodoro">
       <h2>Você está: Trabalhando</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
-        <Button text="Teste" onClick={() => alert("oi")}></Button>
-        <Button text="Teste" onClick={() => alert("oi")}></Button>
-        <Button text="Teste" onClick={() => alert("oi")}></Button>
+        <Button text="Reiniciar" onClick={() => configureWork()}></Button>
+        <Button 
+        text={timeCounting ? 'Pausar' : 'Iniciar'} 
+        onClick={() => setTimeCounting(!timeCounting)}>
+        </Button>
+        <Button text="Voltar" onClick={() => alert('Em construção')}></Button>
       </div>
       <div className="details">
         <p>Teste: testetetete</p>
