2a33e06fb344c27871180b9f2185923bcd54302c
moisesBarsoti <moises.barsot2@gmail.com>
2024-12-16 14:55:34 -0300

    Subindo Arquivos

--- src/pages/PegandoInfo/PegandoInfo.tsx
+++ src/pages/PegandoInfo/PegandoInfo.tsx
@@ -1,24 +1,44 @@
+import { IoIosArrowForward } from "react-icons/io";
 import "./PegandoInfo.css";
+import { MdDelete } from "react-icons/md";
 
 export const PegandoInfo = () => {
   return (
     <div className="container">
-      <form action="">
-        <h1>Preencha as informações</h1>
-        <label htmlFor="">Nome da Tarefa</label>
-        <input type="text" title="Nome da tarefa" autoFocus />
-        <label htmlFor="">Tempo da Tarefa</label>
-        <input type="number" />
-        <label htmlFor="">Tempo de Descanso curto</label>
-        <input type="number" />
-        <label htmlFor="">Tempo de Descanso longo</label>
-        <input type="number" />
-        <label htmlFor="">Ciclos da Tarefa</label>
-        <input type="number" />
-        <a title="Criar pomodoro" href="/temporizador">
-          Criar
-        </a>
-      </form>
+      <div className="info">
+        <form action="">
+          <h1>Preencha as informações</h1>
+          <label htmlFor="">Nome da Tarefa</label>
+          <input type="text" title="Nome da tarefa" autoFocus />
+          <label htmlFor="">Tempo da Tarefa</label>
+          <input type="number" />
+          <label htmlFor="">Tempo de Descanso curto</label>
+          <input type="number" />
+          <label htmlFor="">Tempo de Descanso longo</label>
+          <input type="number" />
+          <label htmlFor="">Ciclos da Tarefa</label>
+          <input type="number" />
+          <a title="Criar pomodoro" href="/temporizador">
+            Criar
+          </a>
+        </form>
+        <div className="pomodorosSave">
+          <h1>Pomodoros Salvos</h1>
+          <div className="salvos">
+            <p>teste</p>
+            <MdDelete
+              className="icon"
+              id="iconDelete"
+              title="Deletar Pomodoro"
+            />
+            <IoIosArrowForward
+              className="icon"
+              id="iconArrow"
+              title="Ir para Pomodoro"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
